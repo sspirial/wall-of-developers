c2b093be0f16d562c9ee53505d9bdd272664d253
sspirial <sspirial@gmail.com>
2025-05-20 16:13:07 +0300

    Add Hackathon and InfoPanel components to App; clean up index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Hackathon from "./hackathon";
+import InfoPanel from "./InfoPanel";
 import Navbar from "./Navbar";
 import { Pitches } from "./Pitches";
 function App() {
   return (
     <>
       <Navbar />
+      <Hackathon />
+      <InfoPanel />
       <Pitches />
+      {/* <Footer /> */}
     </>
   );
 }
