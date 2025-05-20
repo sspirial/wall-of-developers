3271259da1476673ac3b8ed2f13dc6184db20672
JMakoti <josephmwamuye11@gmail.com>
2025-05-20 15:13:21 +0300

    new: navbar & homepage

---
+++ src/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <div>
+            <div className="navbar">
+                <div className="navbar-logo">
+                    <img src="/assets/sph-logo.png" alt="Logo" />
+                </div>
+                <div className="navbar-links">
+                    <Link to="#" className='links' >Home</Link>
+                    <Link to="#" className='links' >Hackathon</Link>
+                    <Link to="#" className='links' >Pannel</Link>
+                    <Link to="#" className='links'>Pitches</Link>
+                    <Link to="#" className='links'>Contacts</Link>
+                </div>
+            </div>
+            <div className="hero">
+                <div className="hero__image">
+                    <img src="/assets/innovation.webp" alt="Hackathon" />
+                </div>
+            </div>
+        </div>
+    )
+}
